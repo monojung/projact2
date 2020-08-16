d1991f97c7f26e7665854f07e8c0a1b1821e8da8
Rithipoom Ariyajon <goldcyber.fenz@gmail.com>
2020-08-16 14:46:44 +0700

    post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index()
     {
         $categories = Post::all();
-        return $categories;
+        return view('post.index');
     }
 
     public function show($id)
